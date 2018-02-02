2ea50fe7e37707dad7ae0e94c57598215eb9a49e
Brent Guistwite <brentguistwite@gmail.com>
2018-02-02 12:48:41 -0500

    Create Reducer Function. Move on to create store.

---
+++ src/reducers/cheese.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { FETCH_CHEESES_ERROR, FETCH_CHEESES_REQUEST, FETCH_CHEESES_SUCCESS } from '../actions/cheese';
+
+const initialState = {
+  cheeses: [],
+  loading: false,
+  error: null,
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+  case FETCH_CHEESES_ERROR:
+    return {
+      ...state,
+      error: action.error,
+      loading: false,
+      cheeses: [],
+    };
+  case FETCH_CHEESES_REQUEST:
+    return {
+      ...state,
+      error: null,
+      loading: true,
+    };
+  case FETCH_CHEESES_SUCCESS:
+    return {
+      ...state,
+      error: null,
+      loading: false,
+      cheeses: [action.cheeses],
+    };
+  default:
+    return state;
+  }
+}
